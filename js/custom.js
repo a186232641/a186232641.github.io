cddc0e96771a8e4e84b92bc9853649fab4e27cfd
FeiLong <a186232641@gmail.com>
2023-08-23 21:37:10 +0800

    Site updated: 2023-08-23 21:37:10

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+var shine = {
+    //跳转按钮
+    toPage: function() {
+        console.log("执行跳转");
+        var e = document.querySelectorAll(".page-number")
+          , t = parseInt(e[e.length - 1].innerHTML)
+          , o = document.getElementById("toPageText")
+          , n = parseInt(o.value);
+        if (!isNaN(n) && n > 0 && "0" !== ("" + n)[0] && n <= t) {
+            var a = (n == 1) ? "/" : "/page/" + n + "/";
+            document.getElementById("toPageButton").href = a
+        }
+    },
+    //监听输入
+    listenToPageInputPress() {
+        var e = document.getElementById("toPageText")
+          , t = document.getElementById("toPageButton");
+        e && (e.addEventListener("keydown", (e=>{
+            13 === e.keyCode && (shine.toPage(),
+            pjax.loadUrl(t.href))
+        }
+        )),
+        e.addEventListener("input", (function() {
+            "" === e.value || "0" === e.value ? t.classList.remove("haveValue") : t.classList.add("haveValue");
+            var o = document.querySelectorAll(".page-number")
+              , n = +o[o.length - 1].innerHTML;
+            +document.getElementById("toPageText").value > n && (e.value = n)
+        }
+        )))
+    }
+  }
+  shine.listenToPageInputPress();
